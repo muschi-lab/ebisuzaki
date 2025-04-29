c5a32ed7590c806c79b4d9216cba6971fbe82c59
Franceso Muschitiello <71844188+muschi-lab@users.noreply.github.com>
2025-04-29 12:09:11 +0100

    ebisuzaki.R

--- ebisuzaki.R
+++ ebisuzaki.R
@@ -1,14 +1,13 @@
 # Based on discussion in "Ebisuzaki, W., 1997: 
 # A Method to Estimate the Statistical Significance of a Correlation When the Data Are Serially Correlated. 
 # J. Climate, 10, 2147?2153, doi: 10.1175/1520-0442(1997)010<2147:AMTETS>2.0.CO;2."
-# INPUT: 
-#      series = some evenly spaced time/space series
-# OUTPUT:
-#       new_series = a pseudorandom variable created by randomly varying the
-#     	phase of each series' Fourier series between 0 and 2pi.  The
-#     	resultant randomly generated series retains the power spectrum of
-#     	the original series (and hence, autocorrelation and variance properties), 
-#       but not the distribution of values.
+# INPUT: series = some evenly spaced time/space series
+# OUTPUT: new_series = a pseudorandom variable created by randomly varying the
+#     	  phase of each series' Fourier series between 0 and 2pi.  The
+#     	  resultant randomly generated series retains the power spectrum of
+#     	  the original series (and hence, autocorrelation and variance properties), 
+#         but not the distribution of values.
+#
 # Note that 'ifft' requires the package 'signal'
 ebisuzaki <- function(series){
   # calculate fft of data
